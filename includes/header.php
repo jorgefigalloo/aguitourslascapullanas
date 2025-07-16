e01ace91bde90b4a803f7e27e1bd6ea9b69ea4af
jorge.figallo <jorge94web@gmail.com>
2025-07-16 10:08:19 -0500

    revisando

--- includes/header.php
+++ includes/header.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aguitours Las Capullanas - Tu Agencia de Viajes Confiable</title>
-    <link rel="stylesheet" href="/agencia-viajes/css/styles.css">
-    <link rel="stylesheet" href="/agencia-viajes/css/stylesCarrusel.css">
-    <link rel="stylesheet" href="/agencia-viajes/css/styleNosotros.css">
-    <link rel="stylesheet" href="/agencia-viajes/css/stylesDestinos.css">
-    <link rel="stylesheet" href="/agencia-viajes/css/stylesDestinosPromos.css">
-    <link rel="stylesheet" href="/agencia-viajes/css/stylesContacto.css">
-    <link rel="stylesheet" href="/agencia-viajes/css/stylesFooter.css">
-    <link rel="icon" type="image/x-icon" href="/images/ico/capullanas.ico">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/stylesCarrusel.css">
+    <link rel="stylesheet" href="css/styleNosotros.css">
+    <link rel="stylesheet" href="css/stylesDestinos.css">
+    <link rel="stylesheet" href="css/stylesDestinosPromos.css">
+    <link rel="stylesheet" href="css/stylesContacto.css">
+    <link rel="stylesheet" href="css/stylesFooter.css">
+    <link rel="icon" type="image/x-icon" href="images/ico/capullanas.ico">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <meta name="description" content="Descubre los mejores destinos con nuestra agencia de viajes. Ofrecemos paquetes personalizados para tus vacaciones.">
     <meta name="keywords" content="agencia de viajes, destinos turísticos, viajes internacionales">
@@ -21,16 +21,24 @@
 <header>
     <nav>
         <div class="logo">
-            <a href="/" class="logo-link"> <img src="/images/capullanas1.jpg" alt="Logo Agencia de Viajes" class="logo-image">
-            </a>
-            <a href="/">Agencia de Viajes "Aguitours Las Capullanas"</a> </div>
-
-        <div class="menu-toggle">
-            ☰
+            <!-- Agregamos la imagen al costado del texto -->
+            <a href="index.php" class="logo-link">
+                <img src="images/capullanas1.jpg" alt="Logo Agencia de Viajes" class="logo-image">
+                </a>
+            <a href="index.php">Agencia de Viajes "Aguitours Las Capullanas"</a>
+             <!-- Agregamos el favicon -->
+         
         </div>
+
+                    <!-- Agrega un botón de hamburguesa -->
+            <div class="menu-toggle">
+                ☰
+            </div>
         <ul class="menu">
-            <li><a href="/">Inicio</a></li> <li><a href="/destinos">Destinos</a></li> <li><a href="/nosotros">Nosotros</a></li>
-            <li><a href="/contacto">Contacto</a></li>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="destinos.php">Destinos</a></li>
+            <li><a href="nosotros.php">Nosotros</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
         </ul>
     </nav>
 </header>
