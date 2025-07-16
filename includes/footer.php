a22f89b8cfa16d71db034768068203c9a3203aba
jorge.figallo <jorge94web@gmail.com>
2025-07-16 11:34:44 -0500

    regresar al inicio

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,16 @@
 <footer>
     <div class="footer-container">
+        <!-- Derechos Reservados -->
         <p class="copyright">
-            &copy; 2025 Agencia de Viajes "Aguitours Las Capullanas". Todos los derechos reservados.
+            © 2025 Agencia de Viajes "Aguitours Las Capullanas". Todos los derechos reservados.
         </p>
 
+        <!-- Contenedor para Redes Sociales y Botón de Contacto -->
         <div class="right-section">
             <p class="copyright">
-                Síguenos en : 
+                Síguenos en:
             </p>
+            <!-- Redes Sociales -->
             <div class="social-icons">
                 <a href="https://www.facebook.com/people/Aguitours-las-capullanas/100054386595848/" target="_blank" aria-label="Facebook">
                     <i class="fab fa-facebook-f"></i>
@@ -20,11 +23,13 @@
                 </a>
             </div>
 
-            <a href="/contacto" class="cta-button">Contáctanos</a>
+            <!-- Botón de Contacto -->
+            <a href="contacto.php" class="cta-button">Contáctanos</a>
         </div>
     </div>
 </footer>
-<script src="/agencia-viajes/js/scripts.js"></script>
-<script src="/agencia-viajes/js/scriptsNosotros.js"></script>
+
+<script src="js/scripts.js"></script>
+<script src="js/scriptsNosotros.js"></script>
 </body>
 </html>
